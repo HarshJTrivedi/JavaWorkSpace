045756fd81a90b880baf70473451e577444970a3
HarshJTrivedi <harshworks94@gmail.com>
2013-08-10 09:12:58 -0700

    commit 1

--- Lab1/src/MainClass.java
+++ Lab1/src/MainClass.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.Scanner; /// does it change???
 public class MainClass {
 	public static void main(String args[]) {
 		LinkedList<String> listObj=new LinkedList<String>();
